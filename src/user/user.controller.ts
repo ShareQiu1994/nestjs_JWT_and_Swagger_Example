cd46f5a3570fbc211977c43cfb82b73c5e65b042
ShareQiu1994 <421419567@qq.com>
2020-01-12 20:59:10 +0800

    add ApiOperation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,7 +9,12 @@ import {
   Put,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags, ApiBearerAuth, ApiParam } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiParam,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { UserAddDTO } from './DTO/user-add-dto';
 import { UserUpdateDTO } from './DTO/user-update-dto';
 
@@ -20,6 +25,7 @@ import { UserUpdateDTO } from './DTO/user-update-dto';
 export class UserController {
   // 获取user列表
   @Get()
+  @ApiOperation({ summary: '获取用户列表' })
   getUser() {
     return [
       {
@@ -39,6 +45,7 @@ export class UserController {
     name: 'id',
     description: '参数id',
   })
+  @ApiOperation({ summary: '获取指定用户' })
   getUserId(@Param('id') id: Number) {
     return {
       name: '刘博方',
@@ -49,6 +56,7 @@ export class UserController {
 
   // 新增用户
   @Post()
+  @ApiOperation({ summary: '新增用户' })
   addUser(@Body() UserAddDTO: UserAddDTO) {
     return {
       UserAddDTO,
@@ -57,6 +65,7 @@ export class UserController {
 
   // 修改用户
   @Put()
+  @ApiOperation({ summary: '修改用户' })
   updateUser(@Body() UserUpdateDTO: UserUpdateDTO) {
     return {
       UserUpdateDTO,
@@ -69,6 +78,7 @@ export class UserController {
     name: 'id',
     description: '参数id',
   })
+  @ApiOperation({ summary: '删除指定用户' })
   deleteUser(@Param('id') id: Number) {
     return {
       name: '刘博方',
